a634eb21b0f5f19234a383cf28994eb278c962c4
z4nkomziry-hash <z4nkomziry@gmail.com>
2026-07-21 15:02:01 +0300

    Update download.module.ts

--- backend/src/modules/download/download.module.ts
+++ backend/src/modules/download/download.module.ts
@@ -1,1 +1,28 @@
-import axios from 'axios';
+import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import { DownloadController } from './download.controller';
+import { DownloadService } from './download.service';
+import { DownloadProcessor } from './download.processor';
+import { MetricsModule } from '../metrics/metrics.module';
+
+@Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'download',
+      defaultJobOptions: {
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 2000,
+        },
+        removeOnComplete: true,
+        removeOnFail: false,
+      },
+    }),
+    MetricsModule,
+  ],
+  controllers: [DownloadController],
+  providers: [DownloadService, DownloadProcessor],
+  exports: [DownloadService],
+})
+export class DownloadModule {}
