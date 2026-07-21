a88c1d8107df64c47af75c5315f8aa713a81fe11
z4nkomziry-hash <z4nkomziry@gmail.com>
2026-07-21 14:38:53 +0300

    Create auth.module.ts

---
+++ backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { EmailService } from './services/email.service';
+import { TokenService } from './services/token.service';
+import { UserModule } from '../user/user.module';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: {
+          expiresIn: configService.get<string>('jwt.expiresIn', '15m'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    JwtRefreshStrategy,
+    GoogleStrategy,
+    EmailService,
+    TokenService,
+  ],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
